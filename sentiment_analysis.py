cbf7b2a3e54d656016f849d1c7993f94d52b4ade
CatalinaPacioni <catalinapacioni@gmail.com>
2025-11-10 16:39:42 -0300

    Añadi nueva versión

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,12 +1,24 @@
-import warnings  #  Permite manejar mensajes de advertencia
-warnings.filterwarnings("ignore") #oculta  las advertencias para que no molesten en pantalla
+import warnings  # Permite manejar mensajes de advertencia
+warnings.filterwarnings("ignore")  # Oculta las advertencias para que no molesten
 
-import logging #controlar los mensajes del sistema (logs)
-logging.getLogger("transformers").setLevel(logging.ERROR)#  Oculta mensajes de transformers
-logging.getLogger("huggingface_hub").setLevel(logging.ERROR) #  Oculta mensajes del repo
-logging.getLogger("torch").setLevel(logging.ERROR)#  Oculta mensajes de PyTorch (usa el CPU/GPU)
+import logging  # Controla los mensajes del sistema (logs)
+logging.getLogger("transformers").setLevel(logging.ERROR)  # Oculta mensajes de transformers
+logging.getLogger("huggingface_hub").setLevel(logging.ERROR)  # Oculta mensajes del repo
+logging.getLogger("torch").setLevel(logging.ERROR)  # Oculta mensajes de PyTorch
 
-from transformers import pipeline #  Importa sentimientos
+# Cargar archivo .env
+load_dotenv()
+
+# Variables de entorno
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+
+if not TELEGRAM_TOKEN:
+    raise ValueError("No se encuentra el TOKEN de Telegram en el archivo .env")
+
+# Instanciar objetos
+bot = tlb.TeleBot(TELEGRAM_TOKEN)
+
+from transformers import pipeline  # Importa el modelo de análisis de sentimiento
 
 # Cargamos el modelo de análisis de sentimiento
 analizador = pipeline(
@@ -14,28 +26,20 @@ analizador = pipeline(
     model="distilbert/distilbert-base-uncased-finetuned-sst-2-english",
     revision="714eb0f"
 )
-# Pedimos el nombre del usuario
-
-nombre = input(" ¡Hola! ¿Cúal es tu nombre? ")
 
 # Pedimos la reseña del usuario
-reseña = input(" Escribí tu reseña sobre el servicio de recolección: ")
+reseña = input("Escribí tu reseña sobre el servicio de recolección: ")
 
 # Ejecutamos el análisis
 resultado = analizador(reseña)[0]
-etiqueta = resultado["label"]     # POSITIVE o NEGATIVE
-puntaje = resultado["score"] * 100  # porcentaje
+puntaje = resultado["score"] * 100  # Convertimos a porcentaje
 
-print(f"\nResultado del análisis:")
-print(f"Sentimiento: {etiqueta}")
-print(f"Confianza: {puntaje:.2f}%")
-
-# Mostramos faces o mensaje de mejora
-if etiqueta == "POSITIVE" and puntaje >= 50:
-    print("😊 Gracias por tu buena reseña!!", nombre)
-
+# Según el puntaje, mostramos el mensaje adecuado
+if puntaje >= 50:
+    print(f"😊 ¡Gracias por tu reseña!")
 else:
     print("😔 Lamentamos que tu experiencia no haya sido buena.")
-    sugerencia = input("¿En qué podemos mejorar?  ")
-    print(f"Gracias por tu comentario, {nombre} . Valoramos tu opinión: '{sugerencia}'")
+    sugerencia = input("¿En qué podemos mejorar? 💬 ")
+    print(f"Gracias por tu comentario. Valoramos tu opinión: '{sugerencia}'")
+
 
