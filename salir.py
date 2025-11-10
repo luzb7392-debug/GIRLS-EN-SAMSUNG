81feb88fc1b03d4a28e39ef5b5d3900d38958cd9
unknown <luzb7392@gmail.com>
2025-11-10 18:59:23 -0300

    Restauro contenido de salir.py

--- salir.py
+++ salir.py
@@ -1,1 +1,10 @@
+from telebot import types
 
+def salir(bot, message):
+    bot.send_message(
+        message.chat.id,
+        "👋 ¡Gracias por usar G-BOT!, esperamos que vuelvas a consultarnos"
+          "🌱Seguimos cuidando el planeta juntos 💚"
+          "💚Infinitas gracias💚",
+        reply_markup=types.ReplyKeyboardRemove()
+    )
